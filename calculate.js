eb7070b9f8a35cc508db3d8a2678dafe81ea04e5
KrishMandaliya18 <mandaliyakrish18@gmail.com>
2025-08-16 17:35:23 +0530

    new calculate app

---
+++ calculate.js
@@ -0,0 +1,29 @@
+let input = document.getElementById("input");
+let buttons = document.querySelectorAll("button");
+
+
+
+buttons.forEach(element => {
+    element.addEventListener("click",(e)=>{
+        console.log(e.target.textContent);
+
+         if (e.target.textContent === "C"){
+          input.innerText = "";
+           }
+          
+    else if( e.target.textContent  === ">"){
+            input.innerText = input.innerText.slice(0,-1);
+           }
+   
+    else if(e.target.textContent === "="){
+
+    input.innerText = eval(input.innerText);
+       }
+    else{
+        input.innerText += e.target.textContent ;
+    }
+
+
+    input.scrollLeft = input.scrollWidth;
+})
+});
